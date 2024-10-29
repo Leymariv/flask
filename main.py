2dfbc2bb109f0339b71b1405ca57da17bdd0e9db
Leymarie Valmon <valmon.leymarie@gmail.com>
2024-10-29 15:24:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ def hello_world():
     return "hello world"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
     #app.run(debug=True)
