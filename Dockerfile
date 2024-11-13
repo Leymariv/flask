7dc45c0e1f1a8fa64baa7c91f226925fa1460b66
Aksel Maud <ayoub.bouhachmoud@gmail.com>
2024-11-13 16:43:04 +0100

    flask to fast api

--- Dockerfile
+++ Dockerfile
@@ -13,4 +13,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 EXPOSE 3000
  
 # run the command
-CMD ["python", "./main.py"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "3000"]
--- main.py
+++ main.py
@@ -1,33 +1,46 @@
-from flask import Flask, request, jsonify
+from fastapi import FastAPI, HTTPException, Request
 import boto3
 import json
+from fastapi.responses import JSONResponse
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route('/send-data', methods=['POST'])
-def send_data():
-    # Assuming JSON data is sent in the request body
-    data = request.json
+def initialize_firehose_client():
+    try:
+        return boto3.client('firehose', region_name='eu-west-1')
+    except Exception as e:
+        raise Exception(f"Error while connecting to Firehose: {e}")
 
-    # Convert data to JSON string format for Firehose
+
+def send_to_firehose(data: dict):
     record = {
         'Data': json.dumps(data) + '\n'  # Firehose expects newline-delimited records
     }
 
+    firehose_client = initialize_firehose_client()
+    
     try:
-        # Initialize Firehose client
-        firehose_client = boto3.client('firehose', region_name='eu-west-1')
-    except Exception as e:
-        return jsonify({'status': 'error', 'message': f"Error while connceting to firehose {e}"}), 500
-    try:
-        # Send data to Firehose
         response = firehose_client.put_record(
-            DeliveryStreamName= 'amplitude-firehose-firehose-stream',
+            DeliveryStreamName='amplitude-firehose-firehose-stream',
             Record=record
         )
-        return jsonify({'status': 'success', 'response': response}), 200
+        return response
     except Exception as e:
-        return jsonify({'status': 'error', 'message': f"Error while writing to firehose {e}"}), 500        
+        raise Exception(f"Error while writing to Firehose: {e}")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+
+@app.post("/send-data")
+async def send_data(request: Request):
+    try:
+        data = await request.json()
+
+        response = send_to_firehose(data)
+
+        return JSONResponse(content={"status": "success", "response": response}, status_code=200)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=3000)
